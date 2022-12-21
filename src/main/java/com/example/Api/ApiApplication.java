181a866b632d44d8863ae261ff16db375502c2ce
Thivya <bgthivya.selvam@gmail.com>
2022-12-21 11:00:33 +0800

    added scheduler to read from a txt file

--- src/main/java/com/example/Api/ApiApplication.java
+++ src/main/java/com/example/Api/ApiApplication.java
@@ -1,26 +1,31 @@
 package com.example.Api;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 //@ComponentScan({"com.delivery.request"})
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-
-public class ApiApplication {
-
+@EnableScheduling
+public class ApiApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
-
-
-
+		
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd");
 		Date date = new Date(System.currentTimeMillis());
-		System.out.println("Launch Time"+ format.format(date));
+		System.out.println("Launch Time "+ format.format(date));
+		
 	}
-
 }
--- src/main/java/com/example/Api/Service/Impl/FileGenerationServiceImpl.java
+++ src/main/java/com/example/Api/Service/Impl/FileGenerationServiceImpl.java
@@ -2,9 +2,15 @@ package com.example.Api.Service.Impl;
 
 import com.example.Api.Service.FileGenerationService;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 @Slf4j
@@ -30,4 +36,26 @@ public class FileGenerationServiceImpl implements FileGenerationService {
 
         return null;
     }
+    
+    @Scheduled(cron = "0/5 * * * * *")
+	public void readTxtFile() throws IOException
+	{
+    	//created a file object and specified the file path to read
+		 File file = new File("C:\\Users\\wenhu\\Downloads\\Hi.txt");
+		 
+		 try {
+			BufferedReader bufferRead
+			 = new BufferedReader(new FileReader(file));
+			
+			String txt;
+	        while ((txt = bufferRead.readLine()) != null)
+	            System.out.println(txt);
+			
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+	        
+	}
+	
 }
